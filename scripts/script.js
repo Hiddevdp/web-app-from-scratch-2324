39771ae43ce4deb8b5bf71ad4c47e77aeb11a4e3
Hiddevdp <hiddevanderplaat@live.nl>
2024-02-06 15:29:38 +0100

    test

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 async function ophalen() {
   const response = await fetch(
-    "https://github.com/Hiddevdp/web-app-from-scratch-2324/blob/main/scripts/1.json"
+    "https://github.com/Hiddevdp/web-app-from-scratch-2324/blob/main/scripts/info.json"
   );
   const json = await response.json();
   console.log(json);
 }
+ophalen();
