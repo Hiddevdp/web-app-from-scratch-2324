05c180067fbeee7b93a0e6d63ebdf745751877e8
Hiddevdp <hiddevanderplaat@live.nl>
2024-02-06 15:16:51 +0100

    small changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
-async function logMovies() {
+async function ophalen() {
   const response = await fetch(
     "https://github.com/Hiddevdp/web-app-from-scratch-2324/blob/main/scripts/1.json"
   );
-  const movies = await response.json();
-  console.log(movies);
+  const json = await response.json();
+  console.log(json);
 }
