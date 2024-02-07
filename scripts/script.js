80f9a5211c9764d686ec529ac7985d9eb0ff86f1
Hiddevdp <hiddevanderplaat@live.nl>
2024-02-07 15:04:56 +0100

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
+var articles = document.querySelectorAll(".card");
+
+function flip(event) {
+  var content = event.currentTarget.querySelector("div");
+  content.classList.toggle("content");
+  var hgroup = event.currentTarget.querySelector("hgroup");
+  hgroup.classList.toggle("opacity");
+}
+
+articles.forEach(function (article) {
+  article.addEventListener("click", flip);
+});
+
 // async function ophalen() {
 //   const response = await fetch("./scripts/info.json");
 //   const json = await response.json();
@@ -5,11 +18,11 @@
 // }
 // ophalen();
 
-fetch("info.json").then(function (response) {
-  return response.json();
-})
-.then(function(info){
-    let placeholder = document.querySelector("#info");
-    let out = "";
-    for(let product of products)
-})
+// fetch("info.json").then(function (response) {
+//   return response.json();
+// })
+// .then(function(info){
+//     let placeholder = document.querySelector("#info");
+//     let out = "";
+//     for(let product of products)
+// })
