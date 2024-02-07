07d58333a840a8f518cfa8a2881a48e7af4e93e5
Hiddevdp <hiddevanderplaat@live.nl>
2024-02-07 10:17:58 +0100

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,15 @@
-async function ophalen() {
-  const response = await fetch("./scripts/info.json");
-  const json = await response.json();
-  console.log(json);
-}
-ophalen();
+// async function ophalen() {
+//   const response = await fetch("./scripts/info.json");
+//   const json = await response.json();
+//   console.log(json);
+// }
+// ophalen();
+
+fetch("info.json").then(function (response) {
+  return response.json();
+})
+.then(function(info){
+    let placeholder = document.querySelector("#info");
+    let out = "";
+    for(let product of products)
+})
