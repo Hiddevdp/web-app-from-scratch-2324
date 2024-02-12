d5565a5826a168d1d79253f3f2955f0cc686313d
Hiddevdp <hiddevanderplaat@live.nl>
2024-02-12 16:13:34 +0100

    json update

--- scripts/script.js
+++ scripts/script.js
@@ -21,20 +21,47 @@ function flip(event) {
 articles.forEach(function (article) {
   article.addEventListener("click", flip);
 });
+
 // eventlistener
 
-// async function ophalen() {
-//   const response = await fetch("./scripts/info.json");
-//   const json = await response.json();
-//   console.log(json);
-// }
-// ophalen();
+async function ophalen() {
+  const response = await fetch("./scripts/info.json");
+  const json = await response.json();
+  console.log(json);
+  insert(json);
+}
+ophalen();
 
-// fetch("info.json").then(function (response) {
-//   return response.json();
-// })
-// .then(function(info){
-//     let placeholder = document.querySelector("#info");
-//     let out = "";
-//     for(let product of products)
-// })
+function insert(json) {
+  Object.values(json.info).forEach((item) => {
+    const info = document.querySelector("#info");
+
+    const firstname = item.firstName;
+    const lastName = item.lastName;
+    const city = item.city;
+    const age = item.age;
+    const favoGenre = item.favoriet_genre;
+    const favoAnimal = item.favouriteAnimal;
+    const image = item.avatar_url;
+    const hobbies = item.hobbies;
+    const bio = item.bio;
+    const truthsAndLies = item.truthsAndLies;
+
+    const html = `<h2>Persoonlijke gegevens</h2>
+            <ul> 
+                <li><label>first Name:</label> ${firstname}</li>
+                <li><label>Last Name:</label> ${lastName}</li>
+                <li><label>City:</label> ${city}</li>
+                <li><label>Age:</label> ${age}</li>
+                <li><label>Favo Music genre:</label> ${favoGenre}
+                <li><label>Favo Animal:</label> ${favoAnimal}</li>
+                <li><label>Hobbies:</label> ${hobbies}</li>
+                <li><label>Truths and lie:</label> ${truthsAndLies}</li>
+                <li><label>Bio:</label> ${item.bio}</li>
+                <li></li>
+            </ul>
+            <img src="${image}" alt="Avatar">`;
+
+    info.insertAdjacentHTML("beforeend", html);
+  });
+}
